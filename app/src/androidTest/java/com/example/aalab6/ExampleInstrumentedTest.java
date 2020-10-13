71bddeae0da3a964bfb64cddcdd7cb55d1699967
DUDEMANPETER <1908664@students.wits.ac.za>
2020-10-13 04:59:07 +0200

    test

--- app/src/androidTest/java/com/example/aalab6/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/aalab6/ExampleInstrumentedTest.java
@@ -1,27 +1,44 @@
 package com.example.aalab6;
 
-import android.content.Context;
+import android.Manifest;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    public void noInternetPermission(){
+        ViewInteraction Display = onView(withId(R.id.textViewOut));
+        Display.check(matches(withText("Before")));
+    }
 
-        assertEquals("com.example.aalab6", appContext.getPackageName());
+    @Rule
+    public GrantPermissionRule grantPermissionRule = GrantPermissionRule.grant(Manifest.permission.INTERNET);
+    @Test
+    public void outputTest() throws InterruptedException {
+        Thread.sleep(1500);
+        ViewInteraction Display = onView(withId(R.id.textViewOut));
+        Display.check(matches(withText(R.string.carsList)));
     }
 }
